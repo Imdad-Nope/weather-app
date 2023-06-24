27897b15e3ead5822b27e7e3c549f059a02a757e
Imdad-Nope/Alauddin <mdalauddinovi62@gmail.com>
2023-06-24 14:32:46 +0600

    added api

--- src/Components/Home/Weather/CurrentWeather.jsx
+++ src/Components/Home/Weather/CurrentWeather.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 const CurrentWeather = () => {
   const [searchCity, setSearchCity] = useState('Dhaka')
-
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
   // Change input field's text
 
   const handleOnChange = e =>{ 
@@ -12,7 +13,14 @@ const CurrentWeather = () => {
   // Search Data
 
   const handleSearch = () =>{
-}
+    const url = `https://api.weatherbit.io/v2.0/current?&city=${searchCity}&key=768ac710a8f04d7a9156dd48019977b5`
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=>{
+      setData(data.data[0]);
+    })
+  }
+  console.log(data);
     return (
     <div>
            <form className="mt-4 w-25 mx-auto d-flex justify-content-center align-items-center mt-5">
@@ -25,8 +33,9 @@ const CurrentWeather = () => {
               <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" />
             </div>
             <div className="text-center">
-                <h2>{searchCity}</h2>
-                <p>Temperature <span>&deg;</span></p>
+                <h2>{data.city_name}</h2>
+                <p>{data.app_temp} <span>&deg;</span></p>
+                <p>Clouds: {data.weather?.description}</p>
     </div>
      </div>
 
