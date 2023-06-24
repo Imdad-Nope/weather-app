2e6b684a65351a688e6a48cc16df92b02206df57
Imdad-Nope/Alauddin <mdalauddinovi62@gmail.com>
2023-06-24 12:38:39 +0600

    search btn and forecast component is done

---
+++ src/Components/Home/Forecast/Forecast.jsx
@@ -0,0 +1,12 @@
+
+const Forecast = () => {
+    return (
+        <div className="container mx-auto mt-5">
+           <div className="text-center">
+           <h4>Next Few days forecast</h4>
+           </div>
+        </div>
+    );
+};
+
+export default Forecast;
--- src/Components/Home/Weather/CurrentWeather.jsx
+++ src/Components/Home/Weather/CurrentWeather.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
 
 const CurrentWeather = () => {
+  const [searchCity, setSearchCity] = useState('Dhaka')
+
+  // Change input field's text
+
+  const handleOnChange = e =>{ 
+  setSearchCity(e.target.value)
+}
+
+  // Search Data
+
+  const handleSearch = () =>{
+}
     return (
     <div>
            <form className="mt-4 w-25 mx-auto d-flex justify-content-center align-items-center mt-5">
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Search your city" />
+                <input onChange={handleOnChange} value={searchCity} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Search your city.." />
               <div>
-              <button type="button" className="btn btn-info">Search</button>
+              <button onClick={handleSearch} type="button" className="btn btn-info">Search</button>
               </div>
             </form>
+            <div className="text-center mt-4">
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" />
+            </div>
+            <div className="text-center">
+                <h2>{searchCity}</h2>
+                <p>Temperature <span>&deg;</span></p>
+    </div>
      </div>
 
     );
