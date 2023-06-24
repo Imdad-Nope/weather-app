1367d63c024e8206e1390e22b39cffcf0f438f78
Imdad-Nope/Alauddin <mdalauddinovi62@gmail.com>
2023-06-24 18:08:13 +0600

    added spinner

--- src/Components/Home/Weather/CurrentWeather.jsx
+++ src/Components/Home/Weather/CurrentWeather.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 const CurrentWeather = () => {
-  const [searchCity, setSearchCity] = useState('Dhaka')
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [searchCity, setSearchCity] = useState('')
+  const [weatherData, setWeatherData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
   // Change input field's text
 
   const handleOnChange = e =>{ 
@@ -13,16 +14,23 @@ const CurrentWeather = () => {
   // Search Data
 
   const handleSearch = () =>{
+    setIsLoading(true)
     const url = `https://api.weatherbit.io/v2.0/current?&city=${searchCity}&key=768ac710a8f04d7a9156dd48019977b5`
     fetch(url)
     .then(res=> res.json())
     .then(data=>{
-      setData(data.data[0]);
+      setWeatherData(data.data[0]);
+      setIsLoading(false)
+      console.log(data);
     })
+    .catch(error => {
+      console.error(error);
+      setIsLoading(false); 
+    });
   }
-  console.log(data);
+  console.log(weatherData);
     return (
-    <div>
+  <div>
            <form className="mt-4 w-25 mx-auto d-flex justify-content-center align-items-center mt-5">
                 <input onChange={handleOnChange} value={searchCity} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Search your city.." />
               <div>
@@ -32,12 +40,27 @@ const CurrentWeather = () => {
             <div className="text-center mt-4">
               <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" />
             </div>
-            <div className="text-center">
-                <h2>{data.city_name}</h2>
-                <p>{data.app_temp} <span>&deg;</span></p>
-                <p>Clouds: {data.weather?.description}</p>
+
+                {/* Spinner is used here */}
+            <div>
+                {
+                  isLoading && <div className="d-flex justify-content-center">
+                  <div className="spinner-border text-info" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                </div>
+                }
+            </div>
+
+      {
+        searchCity &&
+        <div className="text-center">
+                <h2>{weatherData.city_name}</h2>
+                <p>{weatherData.app_temp} <span>&deg;</span></p>
+                <p>Clouds: {weatherData.weather?.description}</p>
+            </div>
+        }    
     </div>
-     </div>
 
     );
 };
